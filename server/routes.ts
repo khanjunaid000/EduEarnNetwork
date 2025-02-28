325a33b9b8351de75abf241df6afdb44c9de5455
khanjunaid000 <junaidrazakhan75@gmail.com>
2025-02-28 18:30:40 +0000

    Initial commit

--- server/routes.ts
+++ server/routes.ts
@@ -609,7 +609,7 @@ export async function registerRoutes(app: Express): Promise<Server> {
       return res.status(401).json({ message: "Unauthorized" });
     }
     next();
-  }, app.static(uploadsDir));
+  }, express.static(uploadsDir));
 
   const httpServer = createServer(app);
   return httpServer;
